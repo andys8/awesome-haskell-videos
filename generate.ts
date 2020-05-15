0ca426c68e82f5b070ee2486d3cb8fe548a95dff
andys8 <andys8@users.noreply.github.com>
2020-05-15 19:55:11 +0200

    Generate: Request and render markdown

--- generate.ts
+++ generate.ts
@@ -7,14 +7,62 @@ if (!youtubeKey) {
   throw Error("YouTube API key has to be passed as argument");
 }
 
-const data = Deno.readTextFileSync(youtubeDataFile);
-const videoIds = data.split("\n");
+const readYouTubeIds = (): string[] =>
+  Deno.readTextFileSync(youtubeDataFile).split("\n");
 
-const partsStr = youtubeParts.join(",");
-const idsStr = videoIds.join(",");
-const url_ = encodeURI(
-  `${youtubeApi}?part=${partsStr}&id=${idsStr}&key=${youtubeKey}`
-);
-const res = await fetch(url_);
-const body = new Uint8Array(await res.arrayBuffer());
-await Deno.stdout.write(body);
+const requestVideoData = async (videoIds: string[] = []) => {
+  const idsStr = videoIds.join(",");
+  const partsStr = youtubeParts.join(",");
+  const url_ = encodeURI(
+    `${youtubeApi}?part=${partsStr}&id=${idsStr}&key=${youtubeKey}`
+  );
+  const res = await fetch(url_);
+  return await res.json();
+};
+
+type YoutubeResponse = {
+  items: Array<YoutubeVideo>;
+};
+
+type YoutubeVideo = {
+  id: string;
+  snippet: {
+    title: string;
+    publishedAt: string;
+    thumbnails: { medium: { url: string } };
+  };
+};
+
+type Video = {
+  id: string;
+  title: string;
+  publishedAt: string;
+  thumbnailUrl: string;
+  linkUrl: string;
+};
+
+const sortVideos = (videos: Video[] = []) =>
+  videos.sort((a, b) => +new Date(b.publishedAt) - +new Date(a.publishedAt));
+
+const comparePublished = (a: Video, b: Video) =>
+  +new Date(b.publishedAt) - +new Date(a.publishedAt);
+
+const youtubeLinkUrl = (id: string) => `https://youtu.be/${id}`;
+
+const toVideoData = (resp: YoutubeResponse): Video[] => {
+  const toVideo = ({ id, snippet }: YoutubeVideo) => ({
+    id,
+    title: snippet.title,
+    publishedAt: snippet.publishedAt,
+    thumbnailUrl: snippet.thumbnails.medium.url,
+    linkUrl: youtubeLinkUrl(id),
+  });
+  const { items = [] } = resp;
+  return items.map(toVideo).sort(comparePublished);
+};
+
+const renderMarkdown = (videos: Video[] = []) =>
+  videos.map(({ title, linkUrl }) => `- [${title}](${linkUrl})`).join("\n");
+
+const response = await requestVideoData(readYouTubeIds());
+console.log(renderMarkdown(toVideoData(response)));
